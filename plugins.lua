4ffedf49c3e096034632645202dc1e1250734e35
ChihYing <spacetime0311@gmail.com>
2023-10-19 14:16:18 +0800

    feat: Setup Rust LSP

--- plugins.lua
+++ plugins.lua
@@ -8,6 +8,24 @@
        },
      },
    },
- }
+   {
+     "neovim/nvim-lspconfig",
+     config = function()
+       require "plugins.configs.lspconfig"
+       require "custom.configs.lspconfig"
+     end,
+   },
+   {
+     "simrat39/rust-tools.nvim",
+     ft = "rust",
+     dependencies = "neovim/nvim-lspconfig",
+     opts = function ()
+       return require "custom.configs.rust-tools"
+     end,
+     config = function(_, opts)
+       require('rust-tools').setup(opts)
+     end
+   },
+}
 
  return plugins
