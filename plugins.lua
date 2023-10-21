3b85a1968d9368d0453a8890cf066b5d9fad906e
ChihYing <spacetime0311@gmail.com>
2023-10-21 21:42:25 +0800

    feat: Setup Python Debugger

--- plugins.lua
+++ plugins.lua
@@ -10,7 +10,8 @@ local plugins = {
         "pyright",
         "mypy",
         "ruff",
-        "black"
+        "black",
+        "debugpy",
       },
     },
   },
@@ -46,6 +47,44 @@ local plugins = {
     end,
   },
   {
+    "rcarriga/nvim-dap-ui",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function(_, opts)
+      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+      require("dap-python").setup(path)
+      require("core.utils").load_mappings("dap_python")
+    end,
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    lazy = false,
+    config = function(_, opts)
+      require("nvim-dap-virtual-text").setup()
+    end,
+  },
+  {
     "rust-lang/rust.vim",
     ft = "rust",
     init = function ()
@@ -63,13 +102,6 @@ local plugins = {
       })
       crates.show()
       require("core.utils").load_mappings("crates")
-    end,
-  },
-  {
-    "theHamsta/nvim-dap-virtual-text",
-    lazy = false,
-    config = function(_, opts)
-      require("nvim-dap-virtual-text").setup()
     end,
   },
   {
