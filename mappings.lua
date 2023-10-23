5eb850e096248a298204d919ac35a9ea57a20148
ChihYing <spacetime0311@gmail.com>
2023-10-23 22:04:28 +0800

    feat: tmux binding

--- mappings.lua
+++ mappings.lua
@@ -38,4 +38,16 @@ M.crates = {
   }
 }
 
+M.general = {
+  n = {
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+  },
+  i = {
+    ["jk"] = { "<ESC>", "escape insert mode" , opts = { nowait = true }},
+  }
+}
+
 return M
